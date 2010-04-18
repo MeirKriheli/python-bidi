35744fd44b5194c22bbec44ed017182d97a995cc
Meir Kriheli <meir@mksoft.co.il>
2010-04-18 17:13:08 +0300

    Initial import

---
+++ README.rst
@@ -0,0 +1,7 @@
+python-bidi
+=================
+
+Pure python `Bi-directional`_ (BiDi) related functions
+
+.. _Bi-directional: http://en.wikipedia.org/wiki/Bi-directional_text
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "python-bidi",
+    version = "0.1",
+    url = 'http://github.com/mksoft/python-bidi',
+    license = 'MIT',
+    description = "BIDI related functions",
+    long_description = read('README.rst') + read('CHANGELOG.rst'),
+
+    author = 'Meir Kriheli',
+    author_email = 'meir@mksoft.co.il',
+
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+
+    install_requires = ['setuptools'],
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
