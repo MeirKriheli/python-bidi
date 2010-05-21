f639c0011b7389ad164383e629528e764ac9fa0b
Meir Kriheli <meir@mksoft.co.il>
2010-05-21 06:21:21 +0300

    X1-X9 rules

--- src/bidi/algorithm.py
+++ src/bidi/algorithm.py
@@ -1,9 +1,7 @@
 from unicodedata import bidirectional, mirrored
-from collections import namedtuple
+from collections import namedtuple, deque
 
 PARAGRAPH_LEVELS = { 'L':0, 'AL':1, 'R': 1 }
-
-ExtendedChar = namedtuple('ExtendedChar', 'ch biditype level')
 
 def bidirectional_uppercase_rtl(ch):
     '''Return the bidirectional type of a unicode char, treating upper
@@ -31,30 +29,116 @@ def paragraph_level(unistr, bidi_char_type_func=bidirectional):
     return PARAGRAPH_LEVELS['L']
 
 def map_unistr(unistr, bidi_char_type_func=bidirectional):
-    '''Return a list of ExtendedChar for each char mapped to it's bidirectional
-    type and embeding level. 
+    '''Return a list of Extended char dicts for each char mapped to it's bidirectional
+    type and embeding level. e.g: 
     
+    [{'ch':'A', biditype:'L, level:None}...]
+
     This will be used by the algorithm implemention for resolving
 
     '''
-    return [ExtendedChar(ch, bidi_char_type_func(ch), None) for ch in unistr]
+    return [{'ch':ch, 'biditype':bidi_char_type_func(ch), 'level':None} \
+            for ch in unistr]
     
 def explicit_embeddings_and_overrides(extended_chars, paragraph_level=None):
     '''Calucalte the levels taking into account Explicit Embeddings and
-    Explicit Overrides.
+    Explicit Overrides. Yields an ExtendedChar with levels set
+    only in case of X6 and X8, thus implementing X9
 
     Implements X1-X9.
 
     '''
+    
+    EXPLICIT_LEVEL_LIMIT = 62
+
+    # if no paragraph_level default to 'L'. This is safe to do here as it
+    # if paragraph_level is None or 0, it still works (0 will remain 0 after
+    #  `or`). We also need to preserve the paragraph_level for X8 rule.
+    #
+    # Rule X1
+    paragraph_level = paragraph_level or PARAGRAPH_LEVELS['L']
+    embedding_level = paragraph_level 
+    directional_override = ''
+
+    queue = deque()
+    overflow_counter = almost_overflow_counter = 0
+
+    least_greater_odd = lambda x: (x + 1) | 1
+    least_greater_even = lambda x: (x + 2) & ~1
+
+    RULES_MAPPINGS = {
+        'RLE': (least_greater_odd, ''),
+        'LRE': (least_greater_even, ''),
+        'RLO': (least_greater_odd, 'R'),
+        'LRO': (least_greater_even, 'L'),
+    }
+    RULES_MAPPINGS_KEYS = RULES_MAPPINGS.keys()
+    
+    # Added 'B' so X6 won't execute in that case and X8 will run it's course
+    RULE_X6_IGNORED = RULES_MAPPINGS_KEYS + ['BN', 'PDF', 'B'] 
 
     for ex_ch in extended_chars:
-        pass
+        biditype = ex_ch['biditype']
 
+        #X2, X3, X4, X5 rules
+        if biditype in RULES_MAPPINGS_KEYS:
+            if overflow_counter != 0:
+                overflow_counter += 1
+                continue
+
+            level_func, override = RULES_MAPPINGS[biditype]
+            new_level = level_func(embedding_level)
+
+            if new_level < EXPLICIT_LEVEL_LIMIT:
+                queue.append( (embedding_level, directional_override) )
+                embedding_level, directional_override = new_level, override
+
+            elif embedding_level == 60:
+                # The new level is invalid, but a valid level can still be
+                # achieved if this level is 60 and we encounter an RLE or 
+                # RLO further on.  So record that we 'almost' overflowed.
+                almost_overflow_counter += 1
+
+            else:
+                overflow_counter += 1
+
+        #X6 rule
+        elif biditype not in RULE_X6_IGNORED:
+            ex_ch['level'] = embedding_level
+            if directional_override:
+                ex_ch.biditype = directional_override
+
+            yield ex_ch
+
+        #X7 rule
+        elif biditype == 'PDF':
+            if overflow_counter:
+                overflow_counter -= 1
+            elif almost_overflow_counter and embedding_level != 61:
+                almost_overflow_counter -= 1
+            elif queue:
+                embedding_level, directional_override = queue.pop()
+        
+        #X8 rule
+        elif biditype == 'B':
+            queue.clear()
+            overflow_counter = almost_overflow_counter = 0
+            embedding_level = ex_ch['level'] =  paragraph_level 
+            directional_override = ''
+
+            yield ex_ch
+
+def resolve_weak_types(extended_chars):
+    pass
 
 if __name__ == '__main__':
     unistr = u'<H123>shalom</H123>'
+    unistr = u'''DID YOU SAY '\u202Ahe said "\u202Bcar MEANS CAR\u202C"\u202C'?'''
     p_level = paragraph_level(unistr, bidirectional_uppercase_rtl)
     
     ex_chars = map_unistr(unistr, bidirectional_uppercase_rtl)
+    ex_chars_with_levels = list(explicit_embeddings_and_overrides(ex_chars, p_level))
 
-    print ex_chars
+    print ''.join([c['ch'] for c in ex_chars_with_levels])
+    print ''.join([str(c['level']) for c in ex_chars_with_levels])
+    print ''.join([str(c['biditype']) for c in ex_chars_with_levels])
