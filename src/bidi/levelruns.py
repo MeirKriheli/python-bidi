4a8aa119b1d44c9bd76b51a147255fd346749e52
Meir Kriheli <meir@mksoft.co.il>
2010-05-23 18:21:53 +0300

    Empty LevenRun class

---
+++ src/bidi/levelruns.py
@@ -0,0 +1,15 @@
+"""Implements the level runs of the algorithm"""
+
+from collections import deque
+from chartypes import TextOrdering
+
+class LevelRun(object):
+    """Implement the level run of the alogrithm, prepending `sor` and `eor`
+    to the list and setting ExChar's next and previous chars"""
+
+    def __init__(self, sor, eor, extended_chars):
+        """Prepends `sor` and appends `eor` to extended_chars"""
+
+        self.chars = deque(extended_chars)
+        self.chars.appendleft( TextOrdering(sor) )
+        self.chars.append( TextOrdering(eor) )
