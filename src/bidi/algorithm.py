f05cafd0f057d684a441b63f4b28818b25bdc309
Meir Kriheli <meir@mksoft.co.il>
2010-05-24 00:53:39 +0300

    Some code to utilize new classes

--- src/bidi/algorithm.py
+++ src/bidi/algorithm.py
@@ -3,6 +3,8 @@
 from unicodedata import bidirectional, mirrored
 from collections import namedtuple, deque
 
+from paragraph import Paragraph
+
 PARAGRAPH_LEVELS = { 'L':0, 'AL':1, 'R': 1 }
 PARAGRAPH_LEVELS_KEYS = PARAGRAPH_LEVELS.keys()
 
@@ -300,16 +302,24 @@ if __name__ == '__main__':
     uni_str = u'<H123>shalom</H123>'
     uni_str = u'''DID YOU SAY '\u202Ahe said "\u202Bcar MEANS CAR\u202C"\u202C'?'''
 
-    p_level = paragraph_level(uni_str, bidirectional_uppercase_rtl)
-    
-    ex_chars = map_unistr(uni_str, bidirectional_uppercase_rtl)
-    ex_chars_with_levels = explicit_embeddings_and_overrides(ex_chars, p_level)
+    #p_level = paragraph_level(uni_str, bidirectional_uppercase_rtl)
+   # 
+    #ex_chars = map_unistr(uni_str, bidirectional_uppercase_rtl)
+    #ex_chars_with_levels = explicit_embeddings_and_overrides(ex_chars, p_level)
 
-    run_levels = list(resolve_runlevels(ex_chars_with_levels, p_level))
-    pprint.pprint(run_levels)
+    #run_levels = list(resolve_runlevels(ex_chars_with_levels, p_level))
+    #pprint.pprint(run_levels)
 
-    run_levels_weak_resolved = [resolve_weak_types(*l) for l in run_levels]
-    pprint.pprint(run_levels_weak_resolved)
+    #run_levels_weak_resolved = [resolve_weak_types(*l) for l in run_levels]
+    #pprint.pprint(run_levels_weak_resolved)
 
-    run_levels_neutral_resolved = [resolve_neutral_types(*l) for l in run_levels_weak_resolved]
-    pprint.pprint(run_levels_neutral_resolved)
+    #run_levels_neutral_resolved = [resolve_neutral_types(*l) for l in run_levels_weak_resolved]
+    #pprint.pprint(run_levels_neutral_resolved)
+
+    p = Paragraph(uni_str, upper_is_rtl=True)
+    p.get_display()
+
+    pprint.pprint(p.storage)
+    for lrun in p.level_runs:
+        pprint.pprint(lrun)
+    print len(p.level_runs), p.level
