b99cc4d10c96df239d381078e0fc527b61e4d941
Meir Kriheli <mkriheli@gmail.com>
2014-08-09 20:27:02 +0300

    Merge setup.py from cookiecutter, remove src dir

--- setup.py
+++ setup.py
@@ -1,35 +1,53 @@
-import os
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
 
 setup(
-    name = "python-bidi",
-    version = "0.3.4",
-    url = 'http://github.com/mksoft/python-bidi',
-    description = 'BiDi layout implementation in pure python',
-    long_description = read('README.rst') + read('CHANGELOG.rst') + read('TODO.rst'),
-    license = 'http://www.gnu.org/licenses/lgpl.html',
-    author = 'Meir Kriheli',
-    author_email = 'meir@mksoft.co.il',
-
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    include_package_data = True,
-
-    install_requires = ['setuptools'],
-
-    entry_points = {
+    name="python-bidi",
+    version="0.3.4",
+    url='http://github.com/mksoft/python-bidi',
+    description='BiDi layout implementation in pure python',
+    long_description=readme + '\n\n' + history,
+    author='Meir Kriheli',
+    author_email='mkriheli@gmail.com',
+    packages=[
+        'bidi',
+    ],
+    package_dir={
+        'bidi': 'bidi'
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={
         'console_scripts': 'pybidi = bidi:main'
     },
-
-    classifiers = [
+    license='http://www.gnu.org/licenses/lgpl.html',
+    zip_safe=False,
+    keywords='bidi unicode layout',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
         'Programming Language :: Python',
         'Topic :: Text Processing',
-    ]
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
 )
