70b18e732f72670c241b77ed9db068e3b4a12101
Meir Kriheli <mkriheli@gmail.com>
2024-07-09 16:29:10 +0300

    Implemented get_display_inner in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,35 @@
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
+use unicode_bidi::{BidiInfo, Level};
+
+#[pyfunction]
+#[pyo3(signature = (text, base_dir=None, debug=false))]
+pub fn get_display_inner(text: &str, base_dir: Option<char>, debug: bool) -> PyResult<String> {
+    let level: Option<Level> = match base_dir {
+        Some('L') => Some(Level::ltr()),
+        Some('R') => Some(Level::rtl()),
+        None => None,
+        _ => return Err(PyValueError::new_err("base_dir can be 'L', 'R' or None")),
+    };
+    let bidi_info = BidiInfo::new(text, level);
+
+    if debug {
+        return Ok(format!("{bidi_info:#?}"));
+    }
+
+    let display: String = bidi_info
+        .paragraphs
+        .iter()
+        .map(|para| {
+            let line = para.range.clone();
+            bidi_info.reorder_line(para, line)
+        })
+        .collect();
+    Ok(display)
+}
 
 #[pymodule]
-fn my_extension(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn bidi(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_display_inner, m)?)?;
     Ok(())
 }
